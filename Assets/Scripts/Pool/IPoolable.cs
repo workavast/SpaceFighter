14503bbf61b407ddd6947393fd032de502f85273
workavast <workavast@yandex.ru>
2023-06-26 18:57:45 +0300

    Impoted Pool

---
+++ Assets/Scripts/Pool/IPoolable.cs
@@ -0,0 +1,15 @@
+using System;
+
+public interface IPoolable<TElement>
+{
+    public event Action<TElement> ReturnElementEvent;
+    public event Action<TElement> DestroyElementEvent;
+    
+    public void OnElementExtractFromPool();
+    public void OnElementReturnInPool();
+}
+
+public interface IPoolable<TElement, TId> : IPoolable<TElement>
+{
+    public TId PoolId { get;}
+}
