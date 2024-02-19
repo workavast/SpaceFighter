20db0ae44f872f8d0f1a8ba24e4f268589d2c834
workavast <workavast@yandex.ru>
2024-02-19 15:22:43 +0300

    desktop: activation GamplayMenu by pressing esc\space

--- Assets/Scripts/UI_System/UI_Screens/Gameplay/GameplayMainScreen.cs
+++ Assets/Scripts/UI_System/UI_Screens/Gameplay/GameplayMainScreen.cs
@@ -1,5 +1,7 @@
 using Controllers;
+using Initializers;
 using Managers;
+using Saves;
 using TMPro;
 using UI_System.Elements;
 using UnityEngine;
@@ -13,10 +15,17 @@ namespace UI_System.UI_Screens.Gameplay
         [SerializeField] private UI_Counter wavesCounter;
         [SerializeField] private UI_Counter killsCounter;
 
+        [SerializeField] private GameObject mainMenu;
+        
         [Inject] private MissionController _missionController;
         [Inject] private WavesManager _wavesManager;
         [Inject] private CoinsManager _coinsManager;
-        
+
+        private void Awake()
+        {
+            mainMenu.SetActive(PlayerGlobalData.PlatformType == PlatformType.Desktop);
+        }
+
         private void Start()
         {
             killsCounter.Init(_missionController.KillsCounter.DestroyedEnemiesCounter);
@@ -25,7 +34,13 @@ namespace UI_System.UI_Screens.Gameplay
             _coinsManager.MoneyStarsCounter.OnChange += UpdateLevelMoneyCount;
             UpdateLevelMoneyCount();
         }
-    
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Escape))
+                _SetScreen(40);
+        }
+
         private void UpdateLevelMoneyCount()
         {
             coinsCounter.text = $"{_coinsManager.MoneyStarsCounter.CurrentValue}";
